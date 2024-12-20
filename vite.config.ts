0e39bb00b14470236fa2376c2a272f31805d554d
liangqi <1739484858@qq.com>
2024-12-20 13:50:11 +0800

    feat: 安装vite-plugin-dts生成dts文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      outDir: "dist",
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/components/index.ts"),
