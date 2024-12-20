1365ad056900e70a363d072cd0cab372e2edf35d
liangqi <1739484858@qq.com>
2024-12-20 09:32:18 +0800

    feat: 去掉自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-// import { path } from "path";
 import { resolve } from "path";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ArcoResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ArcoResolver()],
-    }),
-    Components({
-      resolvers: [
-        ArcoResolver({
-          sideEffect: true,
-        }),
-      ],
-    }),
-  ],
+  plugins: [vue()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/components/index.ts"),
@@ -53,14 +37,6 @@ export default defineConfig({
       {
         find: /\@\//,
         replacement: resolve(__dirname, "../src"),
-      },
-      {
-        find: "assets",
-        replacement: resolve(__dirname, "../src/assets"),
-      },
-      {
-        find: "vue",
-        replacement: "vue/dist/vue.esm-bundler.js", // compile template
       },
     ],
   },
